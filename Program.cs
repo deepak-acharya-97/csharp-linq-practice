03a3537a846ab8010379be17f8d4720a5a37f1af
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-23 10:51:10 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace play_with_simple_linq_queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
